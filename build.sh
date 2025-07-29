fd98977293b290484a540d4de149a0376a603e27
jjj404 <jjaeger4@arizona.edu>
2025-07-29 20:28:05 +0200

    Added build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Builds and uploads the firmware to the microcontroller with ArduinoOTA
+# Requires avahi-utils package `sudo apt install avahi-utils`
+
+# Get the dynamic IP for the microcontroller
+PORT=$(avahi-resolve-host-name -4 Headwind_1.local | awk '{print $2}')
+
+# Build project files if the build flag is passed
+if [ "$1" = "-b" ]; then
+    pio run -v
+fi
+
+# Upload project to the ESP32 on the correct port
+pio run -t upload --upload-port=$PORT -v
